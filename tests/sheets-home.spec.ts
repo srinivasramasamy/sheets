8f044bf0ca87c6be99ea2f7ff5d335f1b7f9f5cd
Srinivas Ramasamy <srinivasramasami@gmail.com>
2024-10-23 06:12:09 -0400

    Add playwright test for sheets home

--- tests/sheets-home.spec.ts
+++ tests/sheets-home.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test } from "@playwright/test";
+import { expect, Locator, test } from "@playwright/test";
 
 test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:3000/");
@@ -8,5 +8,20 @@ test.describe("Sheets Home", () => {
   test("should have title", async ({ page }) => {
     // Then
     await expect(page).toHaveTitle("Sheets");
+  });
+
+  test("should have menu bar visible", async ({ page }) => {
+    // Given
+    const menuBar: Locator = page.locator("#menu-bar");
+
+    // Then
+    await expect(menuBar).toBeVisible();
+  });
+  test("should have sheets navigator visible", async ({ page }) => {
+    // Given
+    const sheetsNavigator: Locator = page.locator("#sheets-navigator");
+
+    // Then
+    await expect(sheetsNavigator).toBeVisible();
   });
 });
