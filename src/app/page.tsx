2cfba11a15ea055c065f477c6039bb70d30cac68
Srinivas Ramasamy <srinivasramasami@gmail.com>
2025-01-16 08:46:21 -0500

    Wrap SheetsHome component with Redux Provider for state management

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+"use client";
+import { Provider } from "react-redux";
 import SheetsHome from "./components/SheetsHome";
+import store from "./state/store";
 
 export default function Home() {
   return (
-    <>
+    <Provider store={store}>
       <SheetsHome />
-    </>
+    </Provider>
   );
 }
