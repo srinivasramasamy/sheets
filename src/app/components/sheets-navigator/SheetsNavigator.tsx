638580bec9593df4140744d003bc13bc7e64c6be
Srinivas Ramasamy <srinivasramasami@gmail.com>
2025-01-16 08:46:59 -0500

    Integrate Redux store into SheetsNavigator component and update tests

--- src/app/components/sheets-navigator/SheetsNavigator.tsx
+++ src/app/components/sheets-navigator/SheetsNavigator.tsx
@@ -1,11 +1,18 @@
+"use client";
+import Sheet from "@/app/types/Sheet";
 import { Plus } from "react-feather";
+import { useSelector } from "react-redux";
 import Tab from "./Tab";
 
 function SheetsNavigator() {
+  const sheets: Sheet[] = useSelector((state) => state.sheet.sheets);
+
   return (
     <div id="sheets-navigator" className="border flex">
       <Plus id="add-sheets" />
-      <Tab name={"Sheet1"} />
+      {sheets.map((sheet) => (
+        <Tab key={sheet.name} name={sheet.name} />
+      ))}
     </div>
   );
 }
