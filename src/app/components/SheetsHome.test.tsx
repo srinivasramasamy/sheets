ae4227be3383ff77654c683ea005b52722bb193c
Srinivas Ramasamy <srinivasramasami@gmail.com>
2025-01-16 08:46:44 -0500

    Wrap SheetsHome component in Redux Provider for testing

--- src/app/components/SheetsHome.test.tsx
+++ src/app/components/SheetsHome.test.tsx
@@ -1,11 +1,17 @@
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import store from "../state/store";
 import SheetsHome from "./SheetsHome";
 
 describe("Sheets Home", () => {
   it("should render menu bar", () => {
     // Given
-    const { container } = render(<SheetsHome />);
+    const { container } = render(
+      <Provider store={store}>
+        <SheetsHome />
+      </Provider>
+    );
 
     // Then
     expect(container.textContent).toContain("MenuBar");
@@ -13,7 +19,11 @@ describe("Sheets Home", () => {
 
   it("should render sheets navigator", () => {
     // Given
-    const { container } = render(<SheetsHome />);
+    const { container } = render(
+      <Provider store={store}>
+        <SheetsHome />
+      </Provider>
+    );
 
     // Then
     expect(container.querySelector("#sheets-navigator")).toBeTruthy();
