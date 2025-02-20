09c87e2e01e3125b66e283880e23d6704d8618fd
Srinivas Ramasamy <srinivasramasami@gmail.com>
2025-02-19 22:05:14 -0500

    Display the name of the selected sheet in the sheets container component

--- src/app/components/SheetsContainer.tsx
+++ src/app/components/SheetsContainer.tsx
@@ -1,7 +1,14 @@
+import { useSelector } from "react-redux";
+import { IRootState } from "../state/store";
+import Sheet from "../types/Sheet";
+
 function SheetsContainer() {
+  const sheet: Sheet = useSelector(
+    (state: IRootState) => state.sheet.sheets[state.sheet.selectedSheetIndex]
+  );
   return (
     <div id="sheets-container" className="border flex-grow">
-      SheetsContainer
+      {sheet.name}
     </div>
   );
 }
