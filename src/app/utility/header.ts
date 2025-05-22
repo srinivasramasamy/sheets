39ba3e2bd6d2700fc29aefbf54eab9aadaa55a91
Srinivas Ramasamy <srinivasramasami@gmail.com>
2025-05-21 22:23:56 -0400

    Refactor header utility functions and tests; consolidate columnHeader logic and add columnHeaders function

--- src/app/utility/header.ts
+++
@@ -1,9 +0,0 @@
-export const columnHeader = (index: number) => {
-  let header = "";
-  while (index >= 0) {
-    const remainder = index % 26;
-    header = String.fromCharCode(65 + remainder) + header;
-    index = Math.floor(index / 26) - 1;
-  }
-  return header;
-};
